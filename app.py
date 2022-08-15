fd1a3ac64d984976624f06ec3ef379a3736b8821
Bikram Sahoo <61271598+bikram33206@users.noreply.github.com>
2022-08-16 01:27:15 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -3,16 +3,19 @@
 Spyder Editor
 
 This is a temporary script file.
+Bikram.Sahoo@Evalueserve.com 
 """
 import streamlit as st
 # To make things easier later, we're also importing numpy and pandas for
 # working with sample data.
 import numpy as np
 import pandas as pd
-from sklearn.externals import joblib 
+#from sklearn.externals import joblib 
+import joblib
+import joblib
   
 # Load the model from the file 
-Boston = joblib.load('boston.pkl')  
+Boston = joblib.load(r'C:\Users\Bikram.Sahoo\Documents\Tech_wed\boston-house-price-prediction--master\boston-house-price-prediction--master\boston.pkl')  
   
 # Use the loaded model to make predictions 
 #Boston.predict(X_test) 
@@ -27,7 +30,22 @@ def predict_price(LASAT,RM,INDUS,PTRATIO):
 
 def main():
     
-   
+    def add_bg_from_url():
+        st.markdown(
+                f"""
+                <style>
+                .stApp {{
+                    background-image: url("https://lc.zoocdn.com/9f2d741927ffa878efa9a4ffa181dfcb119f7fc3.jpg\");
+                    background-attachment: fixed;
+                    background-size: cover
+                }}
+                </style>
+                """,
+                unsafe_allow_html=True
+            )
+
+    add_bg_from_url() 
+
     html_temp = """
     <div style="background-color:black;padding:10px">
     <h2 style="color:white;text-align:center;">Boston House Price Prediction ML App </h2>
@@ -38,7 +56,7 @@ def main():
     """
     
     
-    st.markdown('<style>body{background-color:tomato;}</style>',unsafe_allow_html=True)
+    #st.markdown('<style>body{background-color:tomato;}</style>',unsafe_allow_html=True)
     st.markdown(html_temp,unsafe_allow_html=True)
     
     LASAT=st.number_input('lower status of the population in %')
@@ -49,8 +67,8 @@ def main():
     
     if st.button("Predict"):
         result=predict_price(LASAT,RM,INDUS,PTRATIO)  
-        
-    st.success('House price is: {} k$'.format(result[0]))    
+        st.success('House price is: {} k$'.format(abs(result[0].round(2)))) 
+       
     
     
     
